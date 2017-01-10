fd66989aa0f830e9cfd979518248b7aee35dac7a
GabrielTM13 <panizkg@gmail.com>
2017-01-10 14:43:16 -0200

    Update MonthCard.lua

--- MonthCard.lua
+++ MonthCard.lua
@@ -1,17 +1,17 @@
 (function()card={
-[1]={reward={{10003,100},{10004,200},{10188,1},{10189,1},{10054,1}},needYuanbao=388,detail=[[vô]],addExpPercent=0,buffId={0},addYinliangPer=0},
-[2]={reward={{10180,1},{10181,1},{10182,1},{11101,2},{10108,1}},needYuanbao=1288,detail=[[<font color='#FFFF00'>chí tôn nguyệt tạp chuyên hưởng đặc quyền </font>
+[1]={reward={{10003,100},{10004,200},{10188,1},{10189,1},{10054,1}},needYuanbao=388,detail=[[Não]],addExpPercent=0,buffId={0},addYinliangPer=0},
+[2]={reward={{10180,1},{10181,1},{10182,1},{11101,2},{10108,1}},needYuanbao=1288,detail=[[<font color='#FFFF00'>Mês extrema cartão de privilégio exclusivo</font>
 
-<font color='#00FF00'>1.đặc quyền chiến đấu thuộc tính:</font>
-  lực công kích +200
-  lực phòng ngự +100
-  sinh mệnh giá trị +2000
-<font color='#00FF00'>2.giết quái thu được kinh nghiệm </font><font color='#FF00FF'>+30%</font><font color='#00FF00'>；</font>
-<font color='#00FF00'>3.giết quái thu được ngân lượng </font><font color='#FF00FF'>+30%</font><font color='#00FF00'>；</font>
-<font color='#00FF00'>4.vô hạn thứ miễn phí rất nhanh truyền tống ；</font>
-<font color='#00FF00'>5.quá quan trảm tướng có thể sử dụng rất nhanh càn quét ；</font>
-<font color='#00FF00'>6.lần đầu kích hoạt miễn phí mở ra 48cái túi đeo lưng cách ；</font>
-<font color='#00FF00'>7.hằng ngày nhiệm vụ và treo giải thưởng nhiệm vụ nhưng miễn phí lập tức hoàn thành ；</font>
-<font color='#00FF00'>8.Khu vực luyện công nhưng miễn phí sống lại </font><font color='#EE7700'>（tân dã vùng ngoại ô 、dã ngoại BOSSđịa đồ ngoại trừ ）</font><font color='#00FF00'>；</font>
-<font color='#00FF00'>9.phó bản nhưng miễn phí sống lại </font><font color='#EE7700'>（quá quan trảm tướng 、vô song chiến vực ngoại trừ ）</font><font color='#00FF00'>；</font>
+<font color='#00FF00'>1. Privilégios lutando propriedades:</font>
+   Ataque+200
+   Defesa+100
+   Vida+2000
+<font color='#00FF00'>2.Matar monstros para ganhar experiência</font><font color='#FF00FF'>+30%</font><font color='#00FF00'>；</font>
+<font color='#00FF00'>3.Shaguai obter prata</font><font color='#FF00FF'>+30%</font><font color='#00FF00'>；</font>
+<font color='#00FF00'>4.Transferência rápida gratuito e ilimitado；</font>
+<font color='#00FF00'>5.Através dos ensaios pode ser usado para limpar rapidamente；</font>
+<font color='#00FF00'>6.Primeiro ativação gratuita de estrutura aberta 48 mochila；</font>
+<font color='#00FF00'>7.Tarefas diárias e tarefas recompensa pode ser concluída imediatamente livre；</font>
+<font color='#00FF00'>8.Mapa de campo pode Ressurreição livre</font><font color='#EE7700'>（Exceto para o novo campo nos arredores do mapa BOSS selvagem）</font><font color='#00FF00'>；</font>
+<font color='#00FF00'>9.As cópias estão disponíveis gratuitamente ressurreição</font><font color='#EE7700'>（Exceto pelas provas, campo de batalha sem precedentes）</font><font color='#00FF00'>；</font>
 ]],addExpPercent=3000,buffId={210,211,212},addYinliangPer=3000}}end)();
